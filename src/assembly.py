d98002800a7378a56ab18af3773d83f106c2d41c
AbyxDev <ken@abyx.dev>
2024-07-15 01:47:38 -0400

    Implement assembly for unary reduction operators

--- src/assembly.py
+++ src/assembly.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import re
 from collections.abc import Iterator
 from typing import cast
 from nbt_structure_utils import Cuboid, NBTStructure, Vector
@@ -31,15 +32,46 @@ def MC_ADFF31(WIDTH: int, ARST_VALUE: int) -> NBTStructure:
         )
     return struct
 
+def MC_UAND16(WIDTH: int) -> NBTStructure:
+    width = max(2, WIDTH * 2 - 1) # 2 blocks per bit - 1, but 1 bit needs 2
+    return clone_clamp(NBTStructure(), 'structures/mc_uand16.nbt', width)
+
+def MC_UNOR16(WIDTH: int) -> NBTStructure:
+    width = WIDTH * 2 - 1 # 2 blocks per bit - 1
+    return clone_clamp(NBTStructure(), 'structures/mc_unor16.nbt', width)
+
+def MC_UOR16(WIDTH: int) -> NBTStructure:
+    width = WIDTH * 2 - 1 # 2 blocks per bit - 1
+    return clone_clamp(NBTStructure(), 'structures/mc_uor16.nbt', width)
+
+def MC_UXOR2() -> NBTStructure:
+    return NBTStructure('structures/mc_uxor2.nbt')
+
+def MC_UXOR4(WIDTH: int) -> NBTStructure:
+    width = WIDTH * 2 - 1 # 2 blocks per bit - 1
+    return clone_clamp(NBTStructure(), 'structures/mc_uxor4.nbt', width)
+
+def MC_UXOR8(WIDTH: int) -> NBTStructure:
+    width = WIDTH * 2 - 1 # 2 blocks per bit - 1
+    return clone_clamp(NBTStructure(), 'structures/mc_uxor8.nbt', width)
+
+def MC_UXOR16(WIDTH: int) -> NBTStructure:
+    width = WIDTH * 2 - 1 # 2 blocks per bit - 1
+    return clone_clamp(NBTStructure(), 'structures/mc_uxor16.nbt', width)
+
 if __name__ == '__main__':
     module = input('Module: ').upper()
-    if module == 'MC_DFF31':
-        width = int(input('WIDTH = '))
-        struct = MC_DFF31(width)
-        struct.get_nbt().write_file(f'structures/mc_dff{width}_out.nbt')
-    elif module == 'MC_ADFF31':
+    if module == 'MC_ADFF31':
         width = int(input('WIDTH = '))
         arst_value = int(input('ARST_VALUE = '), 0)
         struct = MC_ADFF31(width, arst_value)
         arst_value = format(arst_value, f'0{width}b')
         struct.get_nbt().write_file(f'structures/mc_a{arst_value}dff{width}_out.nbt')
+    elif module in set(
+        'MC_DFF31 MC_UAND16 MC_UNOR16 MC_UOR16 '
+        'MC_UXOR2 MC_UXOR4 MC_UXOR8 MC_UXOR16'.split()
+    ):
+        width = int(input('WIDTH = '))
+        struct: NBTStructure = globals()[module](width)
+        name = re.sub(r'\d+$', '', module.lower())
+        struct.get_nbt().write_file(f'structures/{name}{width}_out.nbt')
